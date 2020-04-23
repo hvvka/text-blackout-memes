0203ade30695839e763a62fb7d0c183c20398195
sebastian_bienert <226086@student.pwr.edu.pl>
2020-04-24 00:50:22 +0200

    mentzen hardcode

--- image_analyzer.py
+++ image_analyzer.py
@@ -1,8 +1,9 @@
 import pytesseract
 from pytesseract import Output
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFilter
 import os
 import json
+import utils
 from rectangle import Rectangle
 
 if os.path.isfile('resources/config.json'):
@@ -31,14 +32,18 @@ def includes_word(data, word):
 def get_slangs_matches(slangs, image_boxes):
     result = []
     for slang in slangs:
-        test = includes_word(image_boxes, slang)
+        test = includes_word(image_boxes, slang.strip().lower().replace(" ", ""))
         if test[0]:
             result.append((slang, test[1]))
     return result
 
 
 def get_boxes_from_image(file_name):
-    d = pytesseract.image_to_boxes(file_name, output_type=Output.DICT)
+    img = Image.open(file_name)
+    img = img.convert('L')                             # grayscale
+    img = img.filter(ImageFilter.MedianFilter())       # a little blur
+    img = img.point(lambda x: 0 if x < 140 else 255)   # threshold (binarize)
+    d = pytesseract.image_to_boxes(img, output_type=Output.DICT)
     n_boxes = len(d['char'])
     result = []
     for i in range(n_boxes):
@@ -47,7 +52,6 @@ def get_boxes_from_image(file_name):
     return result
 
 def get_difference_with(rectangle, visible_rectangles):
-    #print(rectangle)
     processed_rectangle = Rectangle(int(rectangle[0]), int(rectangle[3]), int(rectangle[2]), int(rectangle[1]))
     difference_rects = []
     for visible_rect in visible_rectangles:
@@ -55,10 +59,14 @@ def get_difference_with(rectangle, visible_rectangles):
         difference_rects.extend(list(processed_rectangle - b))
     return set(difference_rects)
 
-
-# THIS SHOULD BE GET CROSSED IMAGE AND USED IN A LOOP WITH ALL POSSIBLE MATCHES
-def show_crossed_image(word, original_image, fill_color):
-    all_boxes = pytesseract.image_to_boxes(original_image, output_type=Output.DICT)
+#XD dont even read this
+def show_crossed_image(word, image_path, fill_color):
+    original_image = Image.open(image_path)
+    img = Image.open(image_path)
+    img = img.convert('L')                             # grayscale
+    img = img.filter(ImageFilter.MedianFilter())       # a little blur
+    img = img.point(lambda x: 0 if x < 140 else 255)   # threshold (binarize)
+    all_boxes = pytesseract.image_to_boxes(img, output_type=Output.DICT)
     n_boxes = len(all_boxes['char'])
     all_boxes_set = {(all_boxes['char'][i], all_boxes['left'][i],
                       all_boxes['top'][i], all_boxes['right'][i],
@@ -66,30 +74,35 @@ def show_crossed_image(word, original_image, fill_color):
     marked_letters_set = set(word[1])
     width, height = original_image.size
     draw = ImageDraw.Draw(original_image)
-    #print(all_boxes_set)
+   # print(marked_letters_set)
     for box in all_boxes_set:
         (x1, y2, x2, y1) = (box[1], box[2], box[3], box[4])
         if box not in marked_letters_set:
             difference = get_difference_with((box[1], box[2], box[3], box[4]), marked_letters_set)
             #print(difference)
             if len(difference) > 1:
-                print('overlapped:', box[0])
+                #print('overlapped:', box)
                 for diff_rect in difference:
-                    draw.rectangle([(diff_rect.x1, height - diff_rect.y1), (diff_rect.x2, height - diff_rect.y2)], outline = "#00FF00")
+                    #draw.rectangle([(diff_rect.x1, height - diff_rect.y1), (diff_rect.x2, height - diff_rect.y2)], outline = "#00FF00")
+                    draw.rectangle([(diff_rect.x1, height - diff_rect.y1), (diff_rect.x2, height - diff_rect.y2)], fill = fill_color)
             else:
                 for diff_rect in difference:
                     draw.rectangle([(diff_rect.x1, height - diff_rect.y1), (diff_rect.x2, height - diff_rect.y2)], fill = fill_color)
             # for diff_rect in difference:
             #     draw.rectangle([(diff_rect.x1, height - diff_rect.y1), (diff_rect.x2, height - diff_rect.y2)], fill = fill_color)
-        else:
-            print('found:', box[0])
-            draw.rectangle([(x1, height - y1), (x2, height - y2)], outline = "#FF0000")
+       # else:
+           # print('found:', box[0])
+            #draw.rectangle([(x1, height - y1), (x2, height - y2)], outline = "#FF0000")
 
-    original_image.show()
+    return original_image
 
 
 if __name__ == '__main__':
-    all_slangs = ['lewak', 'heh', 'Zawias']
+    all_slangs = utils.read_array('./resources/slangs.txt')
     image_path = "./memes/metzen.png"
     matches = get_slangs_matches(all_slangs, get_boxes_from_image(image_path))
-    show_crossed_image(matches[0], Image.open(image_path), '#16202C')
+    #print(matches[1:5])
+    #utils.save_array(matches[1:5], 'mentzen_matches.txt')
+    for match in matches:
+        image = show_crossed_image(match, image_path, '#16202C')
+        image.save(f"./mentzen/mentzen_{match[0]}.png")
